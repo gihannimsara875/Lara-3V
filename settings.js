f6adec6f952ba6ef060efb578577ecd48bc88fa2
manpakaya <136682134+manpakaya@users.noreply.github.com>
2025-09-01 00:08:29 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "LUwiCTaC#xWmuuoKZfwmxSRrluoJlJzggh8ZNcyMjClTD1de95R8", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "jNJTBQ4J#0TsmtisIGIMwGKsoHklVxsDOHuUQvBhBT1JyPV8nUd0", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
