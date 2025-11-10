44c623fc944611b25abc15c56acfda0ac967ed41
gihannimsara875 <gihannimsara875@gmail.com>
2025-11-10 23:08:33 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "6QRhHLhZ#eDN14ayD9iWdq_J7mGNkFvB8pPn0pRGwoRoR8AMZDL4", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "7dBAAbAD#LAwEu6gOtQYLAGb-YDPnOjg0z443oTtYodntJluBteU", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94768211853",
